67aa0bd7c01272a1d29e2f45fa1ce2e6319bdaf2
Dominic Burger <dominic.burger@geowerkstatt.ch>
2026-07-01 14:46:11 +0200

    Start empty gRPC server with reflection service

--- src/main/java/ch/geowerkstatt/ilitoolswrapper/Main.java
+++ src/main/java/ch/geowerkstatt/ilitoolswrapper/Main.java
@@ -1,3 +1,27 @@
-void main() {
-    IO.println("Hello world");
+package ch.geowerkstatt.ilitoolswrapper;
+
+import io.grpc.protobuf.services.ProtoReflectionServiceV1;
+
+import java.io.IOException;
+
+public final class Main {
+    private Main() { }
+
+    /**
+     * Application entry point.
+     */
+    static void main() throws InterruptedException, IOException {
+        final IlitoolsWrapperServer server = new IlitoolsWrapperServer(
+                getPort(),
+                ProtoReflectionServiceV1.newInstance()
+        );
+        server.start();
+        server.blockUntilShutdown();
+    }
+
+    private static int getPort() {
+        final int defaultPort = 5555;
+        final String portEnv = System.getenv("GRPC_PORT");
+        return portEnv != null && !portEnv.isEmpty() ? Integer.parseInt(portEnv) : defaultPort;
+    }
 }
