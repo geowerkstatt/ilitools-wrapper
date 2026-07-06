fc6acfb060d007501eabf67dc46c2742a735d9cd
Dominic Burger <dominic.burger@geowerkstatt.ch>
2026-07-06 14:33:25 +0200

    Configure processing base directory

--- src/main/java/ch/geowerkstatt/ilitoolswrapper/files/FilesystemFileManager.java
+++ src/main/java/ch/geowerkstatt/ilitoolswrapper/files/FilesystemFileManager.java
@@ -5,22 +5,35 @@ import java.io.IOException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 
 public final class FilesystemFileManager implements FileManager {
+    private final Path basePath;
+
+    /**
+     * Creates a new {@link FilesystemFileManager} that uses the directory specified by the environment variable
+     * {@code PROCESSING_DIR} or its fallback as the base directory for {@link ProcessingFile} entries.
+     */
+    public FilesystemFileManager() {
+        String basePath = System.getenv("PROCESSING_DIR");
+        if (basePath == null || basePath.isEmpty()) {
+            basePath = "processing";
+        }
+        this.basePath = Path.of(basePath);
+    }
+
     @Override
     public ProcessingFile createProcessingFile(String folderName, String fileName, String fileExtension) {
         checkFileExtension(fileExtension);
 
-        Path filePath = Paths.get(folderName, fileName + "." + fileExtension);
+        Path filePath = basePath.resolve(folderName, fileName + "." + fileExtension);
         return new FilesystemProcessingFile(filePath);
     }
 
     @Override
     public void deleteProcessingFiles(String folderName) throws IOException {
-        Path path = Paths.get(folderName);
+        Path path = basePath.resolve(folderName);
         Files.walkFileTree(path, new SimpleFileVisitor<>() {
             @Override
             @Nonnull
