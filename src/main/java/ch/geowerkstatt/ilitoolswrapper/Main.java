a81d77aa848f21cebfa9e0b64cb24e028a1dedd3
Joel Wälti <joel.waelti@geowerkstatt.ch>
2026-07-30 14:46:14 +0200

    Register and document ilivalidator service

--- src/main/java/ch/geowerkstatt/ilitoolswrapper/Main.java
+++ src/main/java/ch/geowerkstatt/ilitoolswrapper/Main.java
@@ -4,6 +4,7 @@ import ch.geowerkstatt.ilitoolswrapper.files.FileManager;
 import ch.geowerkstatt.ilitoolswrapper.files.FilesystemFileManager;
 import ch.geowerkstatt.ilitoolswrapper.healthcheck.ServiceHealthCheckManager;
 import ch.geowerkstatt.ilitoolswrapper.ili2gpkg.Ili2gpkgService;
+import ch.geowerkstatt.ilitoolswrapper.ilivalidator.IlivalidatorService;
 import ch.geowerkstatt.ilitoolswrapper.runner.IlitoolsProcessRunner;
 import ch.geowerkstatt.ilitoolswrapper.runner.IlitoolsRunner;
 import io.grpc.protobuf.services.ProtoReflectionServiceV1;
@@ -20,16 +21,19 @@ public final class Main {
         final FileManager fileManager = new FilesystemFileManager();
         final IlitoolsRunner ilitoolsRunner = new IlitoolsProcessRunner();
         final Ili2gpkgService ili2gpkgService = new Ili2gpkgService(fileManager, ilitoolsRunner);
+        final IlivalidatorService ilivalidatorService = new IlivalidatorService(fileManager, ilitoolsRunner);
 
         final ServiceHealthCheckManager serviceHealthCheckManager = new ServiceHealthCheckManager(
-                ili2gpkgService
+                ili2gpkgService,
+                ilivalidatorService
         );
 
         final IlitoolsWrapperServer server = new IlitoolsWrapperServer(
                 getPort(),
                 ProtoReflectionServiceV1.newInstance(),
                 serviceHealthCheckManager.getHealthService(),
-                ili2gpkgService
+                ili2gpkgService,
+                ilivalidatorService
         );
         server.closeOnShutdown(serviceHealthCheckManager);
         server.start();
